dba16e53064b9bb6fabefb2f4d96c0ffc1ed61fd
Mrugan Kulkarni <mrugan.kulkarni63@gmail.com>
2026-03-27 23:02:19 +0530

    [Mrugan Kulkarni] Added: UC11: Provide User Friendly Error Response in case validation fails

---
+++ src/main/java/com/example/employeepayrollapp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.example.employeepayrollapp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationException(MethodArgumentNotValidException ex) {
+
+        Map<String, String> fieldErrors = new HashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                fieldErrors.put(error.getField(), error.getDefaultMessage())
+        );
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", 400);
+        response.put("message", "Validation Failed");
+        response.put("errors", fieldErrors);
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
